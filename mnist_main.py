21a5ebc6dae2c31d3fb1617ce0e117215785d280
timvanharen <timvanharen@live.com>
2025-04-09 18:43:59 +0200

    Mnist sort of working

--- mnist_main.py
+++ mnist_main.py
@@ -130,6 +130,103 @@ class DynamicPadCat(nn.Module):
         return torch.cat([x1, x2], dim=1)
 
 class ScoreNet(nn.Module):
+  """A time-dependent score-based model built upon U-Net architecture."""
+
+  def __init__(self, marginal_prob_std, channels=[32, 64, 128, 256], embed_dim=256):
+    """Initialize a time-dependent score-based network.
+
+    Args:
+      marginal_prob_std: A function that takes time t and gives the standard
+        deviation of the perturbation kernel p_{0t}(x(t) | x(0)).
+      channels: The number of channels for feature maps of each resolution.
+      embed_dim: The dimensionality of Gaussian random feature embeddings.
+    """
+    super().__init__()
+    # Gaussian random feature embedding layer for time
+    self.embed = nn.Sequential(GaussianFourierProjection(embed_dim=embed_dim),
+         nn.Linear(embed_dim, embed_dim))
+    # Encoding layers where the resolution decreases
+    self.conv1 = nn.Conv2d(1, channels[0], 3, stride=1, bias=False)
+    self.dense1 = Dense(embed_dim, channels[0])
+    self.gnorm1 = nn.GroupNorm(4, num_channels=channels[0])
+    self.conv2 = nn.Conv2d(channels[0], channels[1], 3, stride=2, bias=False)
+    self.dense2 = Dense(embed_dim, channels[1])
+    self.gnorm2 = nn.GroupNorm(32, num_channels=channels[1])
+    self.conv3 = nn.Conv2d(channels[1], channels[2], 3, stride=2, bias=False)
+    self.dense3 = Dense(embed_dim, channels[2])
+    self.gnorm3 = nn.GroupNorm(32, num_channels=channels[2])
+    self.conv4 = nn.Conv2d(channels[2], channels[3], 3, stride=2, bias=False)
+    self.dense4 = Dense(embed_dim, channels[3])
+    self.gnorm4 = nn.GroupNorm(32, num_channels=channels[3])    
+
+    # Decoding layers where the resolution increases
+    self.tconv4 = nn.ConvTranspose2d(channels[3], channels[2], 3, stride=2, bias=False)
+    self.dense5 = Dense(embed_dim, channels[2])
+    self.tgnorm4 = nn.GroupNorm(32, num_channels=channels[2])
+    self.tconv3 = nn.ConvTranspose2d(channels[2] + channels[2], channels[1], 3, stride=2, bias=False, output_padding=1)    
+    self.dense6 = Dense(embed_dim, channels[1])
+    self.tgnorm3 = nn.GroupNorm(32, num_channels=channels[1])
+    self.tconv2 = nn.ConvTranspose2d(channels[1] + channels[1], channels[0], 3, stride=2, bias=False, output_padding=1)    
+    self.dense7 = Dense(embed_dim, channels[0])
+    self.tgnorm2 = nn.GroupNorm(32, num_channels=channels[0])
+    self.tconv1 = nn.ConvTranspose2d(channels[0] + channels[0], 1, 3, stride=1)
+    
+    # The swish activation function
+    self.act = lambda x: x * torch.sigmoid(x)
+    self.marginal_prob_std = marginal_prob_std
+  
+  def forward(self, x, t): 
+    # Obtain the Gaussian random feature embedding for t   
+    embed = self.act(self.embed(t))    
+    # Encoding path
+    h1 = self.conv1(x)    
+    ## Incorporate information from t
+    h1 += self.dense1(embed)
+    ## Group normalization
+    h1 = self.gnorm1(h1)
+    h1 = self.act(h1)
+    h2 = self.conv2(h1)
+    h2 += self.dense2(embed)
+    h2 = self.gnorm2(h2)
+    h2 = self.act(h2)
+    h3 = self.conv3(h2)
+    h3 += self.dense3(embed)
+    h3 = self.gnorm3(h3)
+    h3 = self.act(h3)
+    h4 = self.conv4(h3)
+    h4 += self.dense4(embed)
+    h4 = self.gnorm4(h4)
+    h4 = self.act(h4)
+    if PRINT_SIZE:
+        print("t.shape:", t.shape)
+        print("x.shape:", x.shape)  
+        print("embed.shape:", embed.shape)
+        print("h1.shape:", h1.shape)
+        print("h2.shape:", h2.shape)
+        print("h3.shape:", h3.shape)
+        print("h4.shape:", h4.shape)
+        
+    # Decoding path
+    h = self.tconv4(h4)
+    ## Skip connection from the encoding path
+    h += self.dense5(embed)
+    h = self.tgnorm4(h)
+    h = self.act(h)
+    h = self.tconv3(torch.cat([h, h3], dim=1))
+    h += self.dense6(embed)
+    h = self.tgnorm3(h)
+    h = self.act(h)
+    h = self.tconv2(torch.cat([h, h2], dim=1))
+    h += self.dense7(embed)
+    h = self.tgnorm2(h)
+    h = self.act(h)
+    h = self.tconv1(torch.cat([h, h1], dim=1))
+
+    # Normalize output
+    h = h / self.marginal_prob_std(t)[:, None, None, None]
+    return h
+  
+class ScoreNet_poepje(nn.Module):
     """A time-dependent score-based model built upon U-Net architecture."""
 
     def __init__(self, marginal_prob_std, channels=[16, 32, 64, 128, 256], embed_dim=256):
@@ -294,6 +391,7 @@ class ScoreNet(nn.Module):
         if PRINT_SIZE:  print("h.shape: self.tconv1(self.padcat(h, h1))  # Modified", h.shape)
 
         return h / self.marginal_prob_std(t)[:, None, None, None]
+
 
 def marginal_prob_std(t, sigma):
     """Compute the mean and standard deviation of $p_{0t}(x(t) | x(0))$.
@@ -640,31 +738,25 @@ def ode_sampler(score_model,
       * marginal_prob_std(t)[:, None, None, None]
   else:
     init_x = z
-
+    
   shape = init_x.shape
-  # print('init_x.shape:', init_x.shape)
+
   def score_eval_wrapper(sample, time_steps):
     """A wrapper of the score-based model for use by the ODE solver."""
     sample = torch.tensor(sample, device=device, dtype=torch.float32).reshape(shape)
-    # print('sample.shape:', sample.shape)
-    time_steps = torch.tensor(time_steps, device=device, dtype=torch.float32).reshape(batch_size)# .reshape(sample.shape[0], sample.shape[1])
-    # print('time_steps.shape:', time_steps.shape)
-    with torch.no_grad():
-      score = score_model(sample, time_steps) # HERE
-    # print('score.shape:', score.shape)
+    time_steps = torch.tensor(time_steps, device=device, dtype=torch.float32).reshape((sample.shape[0], ))    
+    with torch.no_grad():    
+      score = score_model(sample, time_steps)
     return score.cpu().numpy().reshape((-1,)).astype(np.float64)
-
-  def ode_func(t, x):
+  
+  def ode_func(t, x):        
     """The ODE function for use by the ODE solver."""
-    time_steps = np.ones((shape[0],)) * t
+    time_steps = np.ones((shape[0],)) * t    
     g = diffusion_coeff(torch.tensor(t)).cpu().numpy()
-    # print("score_eval_wrapper(x, time_steps).shape:", score_eval_wrapper(x, time_steps).shape)
     return  -0.5 * (g**2) * score_eval_wrapper(x, time_steps)
-
+  
   # Run the black-box ODE solver.
-  # print("init_x.flatten().cpu().numpy().shape:", init_x.flatten().cpu().numpy().shape)
-  # print("init_x.reshape(-1).cpu().numpy().shape:", init_x.reshape(-1).cpu().numpy().shape)
-  res = integrate.solve_ivp(ode_func, (1., eps), init_x.flatten().cpu().numpy(), rtol=rtol, atol=atol, method='RK45')
+  res = integrate.solve_ivp(ode_func, (1., eps), init_x.reshape(-1).cpu().numpy(), rtol=rtol, atol=atol, method='RK45')  
   print(f"Number of function evaluations: {res.nfev}")
   x = torch.tensor(res.y[:, -1], device=device).reshape(shape)
 
