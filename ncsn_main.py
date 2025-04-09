056d1a05d33fdfead38a61dc07e6693f3fc75f21
obolleurs <obolleurs@gmail.com>
2025-04-09 19:31:43 +0200

    Added new NN model and we hope this works

--- ncsn_main.py
+++ ncsn_main.py
@@ -177,6 +177,157 @@ class DynamicPadCat(nn.Module):
         return torch.cat([x1, x2], dim=1)
 
 class ScoreNet(nn.Module):
+  """A time-dependent score-based model built upon U-Net architecture."""
+
+  def __init__(self, marginal_prob_std, channels=[16, 32, 64, 128, 256, 512], embed_dim=256):
+    """Initialize a time-dependent score-based network.
+
+    Args:
+      marginal_prob_std: A function that takes time t and gives the standard
+        deviation of the perturbation kernel p_{0t}(x(t) | x(0)).
+      channels: The number of channels for feature maps of each resolution.
+      embed_dim: The dimensionality of Gaussian random feature embeddings.
+    """
+    super().__init__()
+    # Gaussian random feature embedding layer for time
+    self.embed = nn.Sequential(GaussianFourierProjection(embed_dim=embed_dim),
+         nn.Linear(embed_dim, embed_dim))
+    # Encoding layers where the resolution decreases
+#Layer 1    
+    self.conv1 = nn.Conv2d(1, channels[0], 3, stride=1, bias=False)
+    self.dense1 = Dense(embed_dim, channels[0])
+    self.gnorm1 = nn.GroupNorm(4, num_channels=channels[0])
+#Layer 2
+    self.conv2 = nn.Conv2d(channels[0], channels[1], 3, stride=2, bias=False)
+    self.dense2 = Dense(embed_dim, channels[1])
+    self.gnorm2 = nn.GroupNorm(32, num_channels=channels[1])
+#Layer 3
+    self.conv3 = nn.Conv2d(channels[1], channels[2], 3, stride=2, bias=False)
+    self.dense3 = Dense(embed_dim, channels[2])
+    self.gnorm3 = nn.GroupNorm(32, num_channels=channels[2])
+#Layer 4
+    self.conv4 = nn.Conv2d(channels[2], channels[3], 3, stride=2, bias=False)
+    self.dense4 = Dense(embed_dim, channels[3])
+    self.gnorm4 = nn.GroupNorm(32, num_channels=channels[3])    
+#Layer 5
+    self.conv5 = nn.Conv2d(channels[3], channels[4], 3, stride=2, bias=False)
+    self.dense5 = Dense(embed_dim, channels[4])
+    self.gnorm5 = nn.GroupNorm(32, num_channels=channels[4])    
+#Layer 6
+    self.conv6 = nn.Conv2d(channels[4], channels[5], 3, stride=2, bias=False)
+    self.dense6 = Dense(embed_dim, channels[5])
+    self.gnorm6 = nn.GroupNorm(32, num_channels=channels[5])    
+
+    # Decoding layers where the resolution increases
+#Decode layer 6    
+    self.tconv6 = nn.ConvTranspose2d(channels[5], channels[4], 3, stride=2, bias=False)
+    self.dense7 = Dense(embed_dim, channels[4])
+    self.tgnorm6 = nn.GroupNorm(32, num_channels=channels[4])
+#Decode layer 5    
+    self.tconv5 = nn.ConvTranspose2d(channels[4] + channels[4], channels[3], 3, stride=2, bias=False, output_padding=1)
+    self.dense8 = Dense(embed_dim, channels[3])
+    self.tgnorm5 = nn.GroupNorm(32, num_channels=channels[3])
+#Decode layer 4    
+    self.tconv4 = nn.ConvTranspose2d(channels[3] + channels[3], channels[2], 3, stride=2, bias=False, output_padding=1)
+    self.dense9 = Dense(embed_dim, channels[2])
+    self.tgnorm4 = nn.GroupNorm(32, num_channels=channels[2])
+#Decode layer 3    
+    self.tconv3 = nn.ConvTranspose2d(channels[2] + channels[2], channels[1], 3, stride=2, bias=False, output_padding=1)    
+    self.dense10 = Dense(embed_dim, channels[1])
+    self.tgnorm3 = nn.GroupNorm(32, num_channels=channels[1])
+#Decode layer 2 
+    self.tconv2 = nn.ConvTranspose2d(channels[1] + channels[1], channels[0], 3, stride=2, bias=False, output_padding=1)    
+    self.dense11 = Dense(embed_dim, channels[0])
+    self.tgnorm2 = nn.GroupNorm(32, num_channels=channels[0])
+#Decode layer 1 
+    self.tconv1 = nn.ConvTranspose2d(channels[0] + channels[0], 1, 3, stride=1)
+    
+    # The swish activation function
+    self.act = lambda x: x * torch.sigmoid(x)
+    self.marginal_prob_std = marginal_prob_std
+  
+  def forward(self, x, t): 
+    # Obtain the Gaussian random feature embedding for t   
+    embed = self.act(self.embed(t))    
+    # Encoding path
+#Layer 1
+    h1 = self.conv1(x)    
+    ## Incorporate information from t
+    h1 += self.dense1(embed)
+    ## Group normalization
+    h1 = self.gnorm1(h1)
+    h1 = self.act(h1)
+#Layer 2
+    h2 = self.conv2(h1)
+    h2 += self.dense2(embed)
+    h2 = self.gnorm2(h2)
+    h2 = self.act(h2)
+#Layer 3
+    h3 = self.conv3(h2)
+    h3 += self.dense3(embed)
+    h3 = self.gnorm3(h3)
+    h3 = self.act(h3)
+#Layer 4
+    h4 = self.conv4(h3)
+    h4 += self.dense4(embed)
+    h4 = self.gnorm4(h4)
+    h4 = self.act(h4)
+#Layer 5
+    h5 = self.conv5(h4)
+    h5 += self.dense5(embed)
+    h5 = self.gnorm5(h5)
+    h5 = self.act(h5)
+#Layer 6
+    h6 = self.conv6(h5)
+    h6 += self.dense6(embed)
+    h6 = self.gnorm6(h6)
+    h6 = self.act(h6)
+
+    if PRINT_SIZE:
+        print("t.shape:", t.shape)
+        print("x.shape:", x.shape)  
+        print("embed.shape:", embed.shape)
+        print("h1.shape:", h1.shape)
+        print("h2.shape:", h2.shape)
+        print("h3.shape:", h3.shape)
+        print("h4.shape:", h4.shape)
+        
+    # Decoding path
+#Decode Layer 6
+    h = self.tconv6(h6)
+    ## Skip connection from the encoding path
+    h += self.dense7(embed)
+    h = self.tgnorm6(h)
+    h = self.act(h)
+#Decode Layer 5
+    h = self.tconv5(torch.cat([h, h5], dim=1))
+    h += self.dense8(embed)
+    h = self.tgnorm5(h)
+    h = self.act(h)
+#Decode Layer 4
+    h = self.tconv4(torch.cat([h, h4], dim=1))
+    h += self.dense9(embed)
+    h = self.tgnorm4(h)
+    h = self.act(h)
+#Decode Layer 3
+    h = self.tconv3(torch.cat([h, h3], dim=1))
+    h += self.dense10(embed)
+    h = self.tgnorm3(h)
+    h = self.act(h)
+#Decode Layer 2
+    h = self.tconv2(torch.cat([h, h2], dim=1))
+    h += self.dense11(embed)
+    h = self.tgnorm2(h)
+    h = self.act(h)
+#Decode Layer 1
+    h = self.tconv1(torch.cat([h, h1], dim=1))
+
+    # Normalize output
+    h = h / self.marginal_prob_std(t)[:, None, None, None]
+    return h
+
+
+class ScoreNet_hatsa(nn.Module):
     """A time-dependent score-based model built upon U-Net architecture."""
 
     def __init__(self, marginal_prob_std, channels=[16, 32, 64, 128, 256], embed_dim=256):
